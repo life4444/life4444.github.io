7de4a939bf058cf578292a35627babe2b1a368bf
Tianze <tcao1@ualberta.ca>
2021-07-13 20:53:50 -0600

    outline

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+var selected = null;
+
+const infantry = new Map([
+    [attack, 10],
+    [defense, 10]
+]);
+
+const units = new Map([
+    ["infantry", infantry]
+]);
+
 function click(){
-    return 4
+    return 4;
 }
 
 function select(id){
-    id.style.border = "4px solid green"
+    if (selected == null){
+        selected = id;
+        id.style.outline = "4px solid green";
+    }
+    else{
+        selected = null;
+        id.style.outline = "none"
+    }
+}
+
+class Unit {
+    constructor(name){
+        this.name = name;
+        this.hp = 100;
+        this.attack = units.get(name).get(attack)
+        this.defense = units.get(name).get(defense)
+    }
+    showStats(block){
+        
+    }
 }
