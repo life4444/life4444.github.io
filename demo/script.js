f226f30634def46ce7b009606820ecec125a67fd
Tianze <tcao1@ualberta.ca>
2021-07-17 18:02:16 -0600

    new structure

---
+++ demo/script.js
@@ -0,0 +1,40 @@
+var selected = null;
+
+const infantry = new Map([
+    ["attack", 10],
+    ["defense", 10]
+]);
+
+const units = new Map([
+    ["infantry", infantry]
+]);
+
+function click(){
+    return 4;
+}
+
+function select(unit, status){
+    if (selected == null){
+        selected = unit;
+        unit.style.outline = "4px solid green";
+        status.style.display = "block";
+    }
+    else{
+        selected = null;
+        unit.style.outline = "none"
+        status.style.display = "none"
+    }
+}
+
+class Unit {
+    constructor(name, master){
+        this.master = master
+        this.name = name;
+        this.hp = 100;
+        this.attack = units.get(name).get("attack")
+        this.defense = units.get(name).get("defense")
+    }
+    showStats(block){
+        
+    }
+}
